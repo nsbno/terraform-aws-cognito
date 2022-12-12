bd05f35c8c5b6d054557c97ae3adb83de8f731c6
Jarle Holtan <jarle.holtan@bekk.no>
2022-12-12 12:59:38 +0100

    Setup Cognito user pool and custom domain

---
+++ main.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  alias  = "certificate_provider"
+  region = "us-east-1"
+}
+
+###############################
+#
+# AWS Cognito User Pool
+#
+###############################
+
+
+resource "aws_cognito_user_pool" "this" {
+  name = var.name_prefix
+
+  admin_create_user_config {
+    allow_admin_create_user_only = true
+  }
+
+  deletion_protection = var.deletion_protection ? "ACTIVE" : "INACTVE"
+
+  tags = var.tags
+}
+
+###############################
+#
+# Custom domain
+#
+###############################
+
+
+resource "aws_cognito_user_pool_domain" "this" {
+  user_pool_id    = aws_cognito_user_pool.this.id
+  domain          = "${var.subdomain_name}.${var.hosted_zone_name}"
+  certificate_arn = module.certificate.arn
+}
+
+data "aws_route53_zone" "this" {
+  name = var.hosted_zone_name
+}
+
+resource "aws_route53_record" "this" {
+  name    = aws_cognito_user_pool_domain.this.domain
+  type    = "A"
+  zone_id = data.aws_route53_zone.this.zone_id
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cognito_user_pool_domain.this.cloudfront_distribution_arn
+    zone_id                = "Z2FDTNDATAQYW2"
+  }
+}
+
+###############################
+#
+# Certificate for custom domain
+#
+###############################
+
+module "certificate" {
+  providers = {
+    aws = aws.certificate_provider
+  }
+  source           = "github.com/nsbno/terraform-aws-acm-certificate?ref=e7988c1"
+  hosted_zone_name = var.hosted_zone_name
+  domain_name      = "${var.subdomain_name}.${var.hosted_zone_name}"
+
+  tags = var.tags
+}
