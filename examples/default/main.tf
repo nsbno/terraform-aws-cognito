7a508b786f2db76e7c7fc540e065535b3cd60f30
Jarle Holtan <jarle.holtan@bekk.no>
2022-12-19 10:14:52 +0100

    Define required provider configurations and Terraform versions

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,5 +1,23 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "use1"
+}
+
 module "user_pool" {
-  source           = "../../"
+  source = "../../"
+
+  providers = {
+    aws.certificate_provider = aws.use1
+  }
+
   name_prefix      = "example"
   subdomain_name   = "example"
   hosted_zone_name = "auth.domain.example.com"
